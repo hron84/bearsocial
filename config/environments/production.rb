d7fc078cde0c75e94d3bf2fa2025342cc879761c
Garami Gábor <hron@hron.me>
2013-03-19 03:30:15 +0100

    Replacing hron.me to herokuapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Jbsocial::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'jbsocial.hron.me' }
+  config.action_mailer.default_url_options = { :host => 'jbsocial.herokuapp.com' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
